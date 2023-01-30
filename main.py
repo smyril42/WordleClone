65c42fb10c1075d38d47cc7578bc3c5eeb5dc1af
smyril42 <merlin@pritlove.org>
2023-01-30 22:20:21 +0100

    fixed MsgOverlays text color not working; set color for loose msg to red

--- main.py
+++ main.py
@@ -241,11 +241,11 @@ class ClickableButton:
 class MsgOverlay:
     def __init__(self, msg: str):
         self.msg = msg
-        self.msg_surface = FONT_BIG.render(self.msg, True, Color.GREEN)
-        self.pos = (SCREEN_SIZE[0] - self.msg_surface.get_size()[0]) / 2, (SCREEN_SIZE[1] - self.msg_surface.get_size()[1]) / 2
         self.visible = False
         self.text_color = Color.BLACK
         self.box_color = Color.LIGHT_GRAY
+        self.msg_surface = FONT_BIG.render(self.msg, True, self.text_color)
+        self.pos = (SCREEN_SIZE[0] - self.msg_surface.get_size()[0]) / 2, (SCREEN_SIZE[1] - self.msg_surface.get_size()[1]) / 2
 
         self.blackout = pg.Surface(SCREEN_SIZE)
         self.blackout.set_alpha(200)
@@ -264,6 +264,7 @@ class MsgOverlay:
 
     def set_text_color(self, rgb):
         self.text_color = rgb
+        self.msg_surface = FONT_BIG.render(self.msg, True, self.text_color)
 
     def _is_visible(func):
         def inner(self, *args, **kwargs):
@@ -308,15 +309,14 @@ def main():
     game_active = True
     clock = pg.time.Clock()
 
-    spacing = round(BOX_SIZE[0] / 2)
-
-    text_boxes = [InputBox((BOX_SIZE[0], (i + 1) * BOX_SIZE[0] + i * spacing), not i) for i in range(COUNT_GUESSES)]
+    text_boxes = [InputBox((BOX_SIZE[0], (i + 1) * BOX_SIZE[0] + i * round(BOX_SIZE[0] / 2)), not i) for i in range(COUNT_GUESSES)]
 
     reset_button = ClickableButton((0, 0), (45, 45), 'reset_icon.png', reset_all)
 
     msg_win = MsgOverlay(f'YOU WIN!')
 
     msg_loose = MsgOverlay(f'YOU LOOSE!')
+    msg_loose.set_text_color(Color.RED)
 
     while game_active:
         print(wordle_engine.used_letters)
