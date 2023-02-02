6be724fdcca08cc16419f47013a25024febf2bf9
smyril42 <merlin@pritlove.org>
2023-02-02 23:46:01 +0100

    added module settings.py holding every user settable constant
    user settable constants:
    - HARD_MODE
    - COUNT_GUESSES
    - WORD_LENGTH
    - VALID_GUESSES_FP
    - VALID_ANSWERS_FP
    - SCREEN_SIZE
    - FONT_SIZE_BIG
    - FONT_SIZE_SMALL

---
+++ settings.py
@@ -0,0 +1,22 @@
+# settings file #
+# all the following variables may be changed carefully
+
+# # #     BASIC    # # #
+
+HARD_MODE: bool = False # default: False
+
+COUNT_GUESSES: int = 6 # default: 6
+
+# # # INTERMEDIATE # # #
+
+WORD_LENGTH: int = 5 # default: 5
+
+VALID_GUESSES_FP: str = 'valid-guesses' # default: 'valid-guesses'
+VALID_ANSWERS_FP: str = 'valid-answers' # default: 'valid-answers'
+
+# # #   ADVANCED   # # #
+
+SCREEN_SIZE: tuple[int, int] = 450, 800 # default: (450, 800)
+
+FONT_SIZE_BIG: int = 53 # default: 53
+FONT_SIZE_SMALL: int = 26 # default: 26
