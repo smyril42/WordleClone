3843766a4063b91b937dc7aa1fd741904d74db16
smyril42 <merlin@pritlove.org>
2023-02-03 02:22:03 +0100

    removed whitespace

--- settings.py
+++ settings.py
@@ -2,20 +2,17 @@
 # all the following variables may be changed carefully
 
 # # #     BASIC    # # #
-
 HARD_MODE: bool = False # default: False
 
 COUNT_GUESSES: int = 6 # default: 6
 
 # # # INTERMEDIATE # # #
-
 WORD_LENGTH: int = 5 # default: 5
 
 VALID_GUESSES_FP: str = 'valid-guesses' # default: 'valid-guesses'
 VALID_ANSWERS_FP: str = 'valid-answers' # default: 'valid-answers'
 
 # # #   ADVANCED   # # #
-
 SCREEN_SIZE: tuple[int, int] = 450, 800 # default: (450, 800)
 
 FONT_SIZE_BIG: int = 53 # default: 53
