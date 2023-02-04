b2b19ee07777c6b0dcb3ecc7b350baf8f378f77f
smyril42 <merlin@pritlove.org>
2023-02-04 14:33:00 +0100

    fixed local typo in clickable_button.py
    - fixed type: diplay_icon -> display_icon

--- clickable_button.py
+++ clickable_button.py
@@ -9,7 +9,7 @@ class ClickableButton:
             display_icon_path, call_onclick):
         self.pos, self.size = pos, size
         self.surface = surface
-        self.diplay_icon = pg.image.load(display_icon_path)
+        self.display_icon = pg.image.load(display_icon_path)
         self.call_onclick = call_onclick
         self.state = const.UNPUSHED
 
@@ -28,9 +28,9 @@ class ClickableButton:
                 self.unpush()
                 self.button_surface.fill(Color.BUTTON_HOVER)
 
-        self.button_surface.blit(self.diplay_icon, (
-            (self.rect.width - self.diplay_icon.get_rect().width) / 2,
-            (self.rect.height - self.diplay_icon.get_rect().height) / 2))
+        self.button_surface.blit(self.display_icon, (
+            (self.rect.width - self.display_icon.get_rect().width) / 2,
+            (self.rect.height - self.display_icon.get_rect().height) / 2))
 
         self.surface.blit(self.button_surface, self.rect)
 
