7b5a8fed1a0f76e3fd1cb634757a9507336d4a29
smyril42 <merlin@pritlove.org>
2023-02-10 21:24:13 +0100

    renamed display_icon to icon

--- clickable_button.py
+++ clickable_button.py
@@ -4,15 +4,15 @@ from constants import Constants as Const
 from colors import Color
 
 class ClickableButton:
-    UNPUSHED = 0
-    PUSHED = 1
+    UNPUSHED = 1
+    PUSHED = 0
     """A button doing something when being clicked"""
     def __init__(
             self, pos, size, surface,
             display_icon_path, call_onclick):
         self.pos, self.size = pos, size
         self.surface = surface
-        self.display_icon = pg.image.load(str(os.path.dirname(__file__)) + '/' + str(display_icon_path))
+        self.icon = pg.image.load(str(os.path.dirname(__file__)) + '/' + str(display_icon_path))
         self.call_onclick = call_onclick
         self.state = self.UNPUSHED
 
@@ -31,9 +31,9 @@ class ClickableButton:
                 self.unpush()
                 self.button_surface.fill(Color.BUTTON_HOVER)
 
-        self.button_surface.blit(self.display_icon, (
-            (self.rect.width - self.display_icon.get_rect().width) / 2,
-            (self.rect.height - self.display_icon.get_rect().height) / 2))
+        self.button_surface.blit(self.icon, (
+            (self.rect.width - self.icon.get_rect().width) / 2,
+            (self.rect.height - self.icon.get_rect().height) / 2))
 
         self.surface.blit(self.button_surface, self.rect)
 
